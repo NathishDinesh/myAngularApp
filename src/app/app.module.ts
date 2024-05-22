90aa40fc6f75addc465a75f8099986823b9c31fa
Nathish <nathish3435@gmail.com>
2024-05-22 23:07:26 +0530

    Navbar layout generic creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './layout/app-layout/app-layout/app-layout.component';
+import { HeaderComponent } from './layout/shared/header/header/header.component';
+import { CuurencyMasterComponent } from './master/Currency Master/cuurency-master/cuurency-master.component';
+import { CurrencyMasterComponent } from './master/currency-master/currency-master.component';
+import { MasterRoutingModule } from './master/currency-master/master-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLayoutComponent,
+    HeaderComponent,
+    CuurencyMasterComponent,
+    CurrencyMasterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MasterRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
